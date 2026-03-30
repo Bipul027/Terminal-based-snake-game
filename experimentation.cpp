dd2213414c12db175a399f5c5b377cc65cb852bc
Bipul Kumar <bipulkumar27av@gmail.com>
2026-03-30 20:17:24 +0530

    initialize repo and experiment with ncurses

---
+++ experimentation.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#include <curses.h>
+
+int main() {
+    // initialize screen
+    initscr();
+
+    // // print to screen
+    // printw("Hello World\n");
+    
+    // // // refresh screen output
+    // // refresh();
+    // mvprintw(10, 20, "Hello World\n");
+
+    // create new window
+    WINDOW *win = newwin(15, 17, 2, 10);
+    refresh();
+
+    // box(win, 1, 0.5);
+
+    // write in window
+    mvwprintw(win, 0, 1, "Hello World\n");
+    wrefresh(win);
+
+    // pause output
+    getch();
+
+    // deallocate memory and end ncurses
+    endwin();
+    return 0;
+}
